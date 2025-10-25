f9934fa7fa988b95a471780472cd5cd2b1be205e
ANUKSHA <anukshakamath26666@gmail.com>
2025-10-25 22:45:37 +0200

    Completed week_0 intermediate assignment

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,1 +1,11 @@
 #YOUR CODE FOR EX_0 INTERMEDIATE HERE
+import math
+
+initial_count = float(input("Enter the initial count:"))
+final_count = float(input("Enter the final count:"))
+time = float(input("Enter the time:"))
+if initial_count > 0 and final_count > 0 and time > 0:
+    growth_rate =(math.log(final_count) - math.log(initial_count)) / time
+    print("The growth rate is", growth_rate)
+else:
+    print("Please type correct value")
